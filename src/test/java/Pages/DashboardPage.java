a23893c2981e8b1bb251b13586f6b32de60e3ae7
haybre <haybreapril@gmail.com>
2026-03-15 18:52:30 +0200

    added DashboardPage and LoginPage locators

--- src/test/java/Pages/DashboardPage.java
+++ src/test/java/Pages/DashboardPage.java
@@ -1,4 +1,17 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class DashboardPage {
+
+WebDriver driver;
+
+@FindBy(xpath ="//*[@id=\"app-main-content\"]/section/div[1]/h2")
+    WebElement DashboardPage;
+
+    public DashboardPage(WebDriver driver){this.driver = driver;
+
+    }
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,23 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    @FindBy(id ="login-email")
+    WebElement emailField;
+    @FindBy(id ="login-password")
+    WebElement passwordField;
+    @FindBy(id ="login-submit" )
+    WebElement loginButton;
+
+    public void enterEmail(String email){emailField.sendKeys("njceles@gmail.com");}
+
+    public void enterPassword(String password){passwordField.sendKeys("@12345678");}
+
+    public void clickLogin(){loginButton.click();}
 }
